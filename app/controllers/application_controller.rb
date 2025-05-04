5be20c5652e3e0c0f9e85ab56117c0e56ead0658
EC2 Default User <ryogotan@ezweb.ne.jp>
2025-05-04 06:55:20 +0000

    ログイン後の遷移先を一覧画面へ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters,if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-      about_path
-  end
+  # ログイン後の遷移先の変更　一覧画面へ遷移するように設定した。
+	def after_sign_in_path_for(resource)
+	  post_images_path
+	end
   
   def after_sign_out_path_for(resource)
     about_path
