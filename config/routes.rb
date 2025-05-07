282c92ff159d771e67d35ea5a82983e1edba8e75
EC2 Default User <ryogotan@ezweb.ne.jp>
2025-05-07 08:08:59 +0000

    コメント投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   # get 'users/show'
   # get 'users/edit'
-  resources :post_images, only: [:new, :index, :show, :create]	
   devise_for :users
   root to: "homes#top"
   get '/homes/about', to:'homes#about', as: "about"
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create]
+	end 
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
