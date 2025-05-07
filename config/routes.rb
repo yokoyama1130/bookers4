7b3e2a17702b5c343f4efa38e04579a4afdfa4c9
EC2 Default User <ryogotan@ezweb.ne.jp>
2025-05-07 07:48:43 +0000

    ユーザー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # リソースの書き方に変更する
+  resources :users, only: [:show, :edit]
+  # get 'users/show'
+  # get 'users/edit'
   resources :post_images, only: [:new, :index, :show, :create]	
   devise_for :users
   root to: "homes#top"
